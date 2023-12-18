b7db84fff97663f873ad82e8dc234c11c9cbc5aa
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-18 17:01:32 +0530

    Print numbers without numbers

---
+++ PRF-Rcap/src/PrintNumbersWithoutNumbers.java
@@ -0,0 +1,16 @@
+/**
+ * @author : Gathsara
+ * created : 12/18/2023 -- 4:58 PM
+ **/
+
+public class PrintNumbersWithoutNumbers {
+    public static void main(String[] args) {
+
+        int one = 'A' / 'A';
+        int hundred ='d';
+
+        for (int i = one; i <= hundred; i++) {
+            System.out.println(i);
+        }
+    }
+}
